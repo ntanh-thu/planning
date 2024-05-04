c176cb96ad059ca36eb0f1dd8918c6b3fd090ef1
Nguyễn Thị Anh Thư <94220054+ntanh-thu@users.noreply.github.com>
2024-05-04 21:43:59 +0700

    add layout dashboard

--- src/pages/Dashboards/index.tsx
+++ src/pages/Dashboards/index.tsx
@@ -1,3 +1,17 @@
+import Header from '@components/Dashboards/Header';
+import MenuLeft from '@components/Dashboards/MenuLeft';
+import { Outlet } from 'react-router-dom';
+
 export const Dashboard = () => {
-   return <div className="dashboard-layout"></div>;
+   return (
+      <div className="dashboard-layout">
+         <MenuLeft />
+         <div className="dashboard-layout-right">
+            <Header />
+            <div className="dashboard-content">
+               <Outlet />
+            </div>
+         </div>
+      </div>
+   );
 };
